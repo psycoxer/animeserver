e7b05590117e866508640b73956b339b9e15aa30
Oaj Bidnurkar <oajrockstar@gmail.com>
2024-11-30 16:07:25 +0530

    UI changes

--- index.js
+++ index.js
@@ -9,38 +9,38 @@ app.use(express.static("public"));
 const anilist = new META.Anilist();
 
 app.get("/", (req, res) => {
-  //   console.log("Hi");
-  res.render("index");
+    //   console.log("Hi");
+    res.render("index");
 });
 
 app.get("/search/:query", (req, res) => {
-  const searchQuery = req.params.query;
+    const searchQuery = req.params.query;
 
-  anilist.search(searchQuery).then((data) => {
-    // res.json(_.map(data.results, 'title'));
-    // res.json(data.results);
-    const result = data.results;
-    res.render("search", { result });
-  });
+    anilist.search(searchQuery).then((data) => {
+        // res.json(_.map(data.results, 'title'));
+        // res.json(data.results);
+        const result = data.results;
+        res.render("search", { result });
+    });
 });
 
 app.get("/anime/:selectedanimeid", (req, res) => {
-  const animeid = req.params.selectedanimeid;
-  anilist.fetchAnimeInfo(animeid).then((data) => {
-    // res.json(data);
-    const episodes = data.episodes;
-    res.render("episodes", { episodes });
-  });
+    const animeid = req.params.selectedanimeid;
+    anilist.fetchAnimeInfo(animeid).then((data) => {
+        // res.json(data);
+        const episodes = data.episodes;
+        res.render("episodes", { episodes });
+    });
 });
 
 app.get("/watch/:id", (req, res) => {
-  const episodeId = req.params.id;
+    const episodeId = req.params.id;
 
-  anilist.fetchEpisodeSources(episodeId).then((data) => {
-    // res.json(data);
-    const sources = data.sources;
-    res.render("watch", { sources });
-  });
+    anilist.fetchEpisodeSources(episodeId).then((data) => {
+        // res.json(data);
+        const sources = data.sources;
+        res.render("watch", { sources });
+    });
 });
 
 app.listen(3000);
